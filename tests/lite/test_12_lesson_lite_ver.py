8e6288f58ccaf29023414d12bf2d1174f7a1408e
Ruslan Antipov <ruslanantipov@gmail.com>
2023-05-24 17:05:31 +0300

    fix

--- tests/lite/test_12_lesson_lite_ver.py
+++ tests/lite/test_12_lesson_lite_ver.py
@@ -8,22 +8,18 @@ def test_successful(setup_browser):
     first_name = "Alex"
     last_name = "Egorov"
 
-    with allure.step("Open registrations form"):
+    with allure.step("Открываем страницу регистрации"):
         browser.open("https://demoqa.com/automation-practice-form")
         browser.element(".practice-form-wrapper").should(have.text("Student Registration Form"))
         browser.driver.execute_script("$('footer').remove()")
         browser.driver.execute_script("$('#fixedban').remove()")
 
-    with allure.step("Fill form"):
+    with allure.step("Заполняем и сохраняем данные студента"):
         browser.element("#firstName").set_value(first_name)
         browser.element("#lastName").set_value(last_name)
         browser.element("#userEmail").set_value("alex@egorov.com")
         browser.element("#genterWrapper").element(by.text("Other")).click()
         browser.element("#userNumber").set_value("1231231230")
-        # browser.element("#dateOfBirthInput").click()
-        # browser.element(".react-datepicker__month-select").s("July")
-        # browser.element(".react-datepicker__year-select").selectOption("2008")
-        # browser.element(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click()
         browser.element("#subjectsInput").send_keys("Maths")
         browser.element("#subjectsInput").press_enter()
         browser.element("#hobbiesWrapper").element(by.text("Sports")).click()
@@ -35,5 +31,5 @@ def test_successful(setup_browser):
         browser.element("#stateCity-wrapper").element(by.text("Delhi")).click()
         browser.element("#submit").click()
 
-    with allure.step("Check form results"):
+    with allure.step("Проверяем соответствие заполненных данных переданным"):
         browser.element("#example-modal-sizes-title-lg").should(have.text("Thanks for submitting the form"))
